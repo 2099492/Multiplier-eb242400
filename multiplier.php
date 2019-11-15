6fac0c41f9723b147d3ed0a3b3ea37f9f6e264c6
2099492 <2099492@talnet.com>
2019-11-15 13:05:01 +0100

    Uitwerking van de exercise

---
+++ multiplier.php
@@ -0,0 +1,8 @@
+<?php
+echo "Ik ga twee getallen met elkaar vermenigvuldigen" . PHP_EOL ;
+echo "Eerste getal?" . PHP_EOL ;
+$eerstegetal = readline() . PHP_EOL ;
+echo "Tweede getal?" . PHP_EOL ;
+$tweedegetal = readline() . PHP_EOL ;
+echo "Uw resultaat is:" . $eerstegetal * $tweedegetal;
+ ?>
